350d285266f32d4e9421620d1195e95b8a978f6d
sankalp1857 <sankalpmehra.mehra@gmail.com>
2016-08-15 16:16:14 +0530

    Add files via upload

---
+++ temp_sensor_call.ino
@@ -0,0 +1,59 @@
+#include<SoftwareSerial.h>
+SoftwareSerial mySerial(9,10);//since we connecting arduino to gsm module via pin 9 and 10 not by rx and tx
+int val;
+int tempPin = 1;
+int buzzer=11;
+int t=0;
+static int x=1;
+float cel;
+void setup()
+{
+ mySerial.begin(2400);   // Setting the baud rate of GSM Module  
+Serial.begin(2400);//Starts serial communication with a baud rate of 9600
+pinMode(buzzer,OUTPUT);
+char phone_no[]="7078869316"
+}
+void Call()
+{
+ mySerial.println("AT");
+ mySerial.print("ATD");
+ mySerial.println(phone_no);
+ mySerial.print(";");
+ delay(20000);
+ mySerial.print("ATH");
+}
+
+void loop()
+{
+  
+  
+val = analogRead(tempPin);
+float mv = ( val/1024.0)*5000; 
+cel = mv/10;
+Serial.print("TEMPRATURE = ");
+Serial.print(cel);
+Serial.print("*C");
+Serial.println();
+delay(500);
+t++;
+Serial.print(t);
+check();
+}
+void check()
+{
+if(cel>25)
+{
+ noTone(11);
+tone(6, 440, 200);
+noTone(6);
+tone(7, 494, 500);
+noTone(7);
+tone(11, 523, 300);
+ while(x==1)
+  {delay(2000);
+    Serial.print("i am in");
+    x++;
+  Call();
+  } 
+}
+}
