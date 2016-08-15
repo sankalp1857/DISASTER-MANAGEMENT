0ae817c8321ee7f61e16d6c8f48131f4824cb87b
sankalp1857 <sankalpmehra.mehra@gmail.com>
2016-08-15 15:38:28 +0530

    Add files via upload

---
+++ temp_sensor.ino
@@ -0,0 +1,66 @@
+#include<SoftwareSerial.h>
+SoftwareSerial mySerial(9,10);//since we connecting arduino to gsm module via pin 9 and 10 not by rx and tx
+int val;
+int tempPin = 1;
+int buzzer=11;
+int t=0;
+static int x=1;
+float cel;
+void setup()
+{
+ mySerial.begin(2400);   // Setting the baud rate of GSM Module  
+Serial.begin(2400);//Starts serial communication with a baud rate of 9600
+pinMode(buzzer,OUTPUT);
+}
+void SendMessage()
+{
+ //mySerial.println("ATD917078869316;");
+  Serial.print("i am in 2");
+ // mySerial.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+  delay(2000);  
+   Serial.print("i am in 3");// Delay of 1000 milli seconds or 1 second
+ // mySerial.println("AT+CMGS=\"+917060328809\"\r"); // Replace x with mobile number
+  delay(2000);
+   Serial.print("i am in 4");
+ //mySerial.println("Your house is on fire...loda lega");// The SMS text you want to send
+  delay(200);
+  Serial.print("i am in 5");
+  // mySerial.println((char)26);// ASCII code of CTRL+Z
+  delay(2000);
+  Serial.print("i am in 6");
+}
+
+void loop()
+{
+  
+  
+val = analogRead(tempPin);
+float mv = ( val/1024.0)*5000; 
+cel = mv/10;
+Serial.print("TEMPRATURE = ");
+Serial.print(cel);
+Serial.print("*C");
+Serial.println();
+delay(500);
+t++;
+Serial.print(t);
+check();
+}
+void check()
+{
+if(cel>25)
+{
+ noTone(11);
+tone(6, 440, 200);
+noTone(6);
+tone(7, 494, 500);
+noTone(7);
+tone(11, 523, 300);
+ while(x==1)
+  {delay(2000);
+    Serial.print("i am in");
+    x++;
+  SendMessage();
+  } 
+}
+}
