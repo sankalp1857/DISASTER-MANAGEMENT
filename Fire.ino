0e5fec1a50014c0ec2f687a23832d8c6c02b43ab
Sankalp Mehra <sankalpmehra.mehra@gmail.com>
2017-04-07 21:33:08 +0530

    Add files via upload

---
+++ Fire.ino
@@ -0,0 +1,14 @@
+#include<SoftwareSerial.h>
+
+int fire = A0;
+int value = 0;
+void setup() {
+  pinMode(fire, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  value = analogRead(fire);
+  Serial.println(value);
+  delay(2000);
+}
---
+++ Gas.ino
@@ -0,0 +1,14 @@
+#include<SoftwareSerial.h>
+float sensor = A0;
+float value;
+
+void setup() {
+  pinMode(sensor, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  value = analogRead(sensor);
+  Serial.println(value);
+  delay(2000);
+}
